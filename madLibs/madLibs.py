30f3bc88afd6bd7ad86d916d5e0235ef06e22e65
grandee3 <166568611+grandee3@users.noreply.github.com>
2024-04-10 12:07:29 -0400

    Add files via upload

---
+++ madLibs/madLibs.py
@@ -0,0 +1,13 @@
+print("Welcome to Mad Libs!")
+
+noun = input("Choose a noun: ")
+
+adjective = input("Choose an adjective: ")
+
+place = input("Choose a place of setting: ")
+
+print ("That reggedy ", noun, " wont stop creeking the old woman said.")
+    
+print ("The old lady went to the ", place, " to go look for her toolbox.")
+
+print ("She finally fixed that old ", adjective, noun, ".")
